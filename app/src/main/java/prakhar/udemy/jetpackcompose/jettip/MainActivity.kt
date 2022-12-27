b44277370978d8b1c12818d28b7d319bfbf62a22
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-27 18:40:39 +0530

    Created TopHeader Composable Function

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import prakhar.udemy.jetpackcompose.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +39,25 @@ fun MyApp(content: @Composable () -> Unit) {
         ) {
             content()
         }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(
+                shape = CircleShape.copy(
+                    all = CornerSize(12.dp)
+                )
+            )
+            //OR------------------------------------------------
+            .clip(RoundedCornerShape(corner = CornerSize(12.dp)))
+    ) {
+
     }
 }
 
