7ecd7bb31ef2760d7a177f17e6f0550daacbb397
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-27 19:11:35 +0530

    Done With TopHeader Composable Function

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
@@ -3,10 +3,8 @@ package prakhar.udemy.jetpackcompose.jettip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,9 +12,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import prakhar.udemy.jetpackcompose.jettip.ui.theme.JetTipTheme
@@ -46,11 +46,12 @@ fun MyApp(content: @Composable () -> Unit) {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
+            .padding(20.dp)
             .clip(
                 shape = CircleShape.copy(
                     all = CornerSize(12.dp)
@@ -60,7 +61,23 @@ fun TopHeader() {
             .clip(RoundedCornerShape(corner = CornerSize(12.dp))),
         color = Color(0xFFE9D7F7)
     ) {
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$$total", style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
     }
 }
 
