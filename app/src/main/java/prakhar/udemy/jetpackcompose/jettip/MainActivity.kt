5fc2cbbc5155ad1f78fbdb45c422e8aa39d1e165
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-28 14:37:45 +0530

    Showing The Input Field

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
@@ -8,17 +8,23 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import prakhar.udemy.jetpackcompose.jettip.components.InputField
 import prakhar.udemy.jetpackcompose.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +32,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                TopHeader()
+//                TopHeader()
+                MainContent()
             }
         }
     }
@@ -77,16 +84,38 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 }
 
 
-@Preview
+//@OptIn(ExperimentalComposeUiApi::class)
+@Preview(showBackground = true)
 @Composable
 fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+//    val keyboardController: LocalSoftwareKeyboardController
     Surface(
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, Color.LightGray)
     ) {
         Column() {
-            Text(text = "Hello Prakhar.....")
+            InputField(
+                valueState = totalBillState,
+                lableId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    //TODO OnValueChanged
+
+//                    keyboardController?.hide()  /Not Working
+                    //This is for to hide keyboard after we are done entering value and pressing enter.
+                }
+            )
         }
     }
 }
