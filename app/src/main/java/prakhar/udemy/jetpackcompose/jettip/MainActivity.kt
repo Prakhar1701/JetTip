c745abd10f28a8351f7345da596523a8115523b2
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-28 16:43:05 +0530

    Shown RoundedIconButton In The App

--- app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jettip/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +14,9 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,12 +25,14 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import prakhar.udemy.jetpackcompose.jettip.components.InputField
 import prakhar.udemy.jetpackcompose.jettip.ui.theme.JetTipTheme
+import prakhar.udemy.jetpackcompose.jettip.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -88,8 +94,7 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 @Preview(showBackground = true)
 @Composable
 fun MainContent() {
-    BillForm(){
-        billAmt ->
+    BillForm() { billAmt ->
         Log.d("Bill Amount", "MainContent: $billAmt")
     }
 }
@@ -116,7 +121,11 @@ fun BillForm(
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, Color.LightGray)
     ) {
-        Column() {
+        Column(
+            modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
             InputField(
                 valueState = totalBillState,
                 lableId = "Enter Bill",
@@ -130,6 +139,30 @@ fun BillForm(
                     //This is for to hide keyboard after we are done entering value and pressing enter.
                 }
             )
+            if (validState) {
+                Row(
+                    modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Split", modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        )
+                    )
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(imageVector = Icons.Default.Remove, onClick = { })
+
+                        RoundIconButton(imageVector = Icons.Default.Add, onClick = { })
+
+                    }
+                }
+            } else {
+                Box {}
+            }
         }
     }
 }
